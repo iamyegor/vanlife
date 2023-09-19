a0aa9402ec4b793535c02ecfc88eff1d241405e5
yegor <astery227@gmail.com>
2023-09-19 10:23:43 +0300

    Firebase initialized

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="home">
       <main className="home__content">
-        <h1 className="home__tagline">You got the travel plans, we got the travel vans.</h1>
+        <h1 className="home__tagline">You got travel plans, we got travel vans</h1>
         <div className="home__description">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</div>
         <button className="home__find-van">Find your plan</button>
       </main>
